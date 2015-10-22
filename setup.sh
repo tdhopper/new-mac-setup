83333a49fe922c55d265662937993e42dbb78bf1
Timothy Hopper <tdhopper@Fisher.local>
2015-10-22 10:40:44 -0400

    Fix miniconda install

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 # Install Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# Install wget
+brew install wget
+
 # Setup Brew bundle for running Brewfiles
 brew tap Homebrew/bundle
 
 # Install Miniconda
-wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
+wget http://repo.continuum.io/miniconda/Miniconda-latest-MacOSX-x86_64.sh -O miniconda.sh
 chmod +x miniconda.sh
 ./miniconda.sh -b
